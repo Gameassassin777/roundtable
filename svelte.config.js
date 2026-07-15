b7eb101624db45533997459faa2e359b20ef2440
Gameassassin777 <gameassassin777@users.noreply.github.com>
2026-07-14 23:10:38 -0500

    version bump to v26

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ export default {
     // gets pinned by GH Pages' CDN max-age=600. Manual bump guarantees the
     // SW sees a different string and triggers the update flow.
     version: {
-      name: '25',
+      name: '26',
       pollInterval: 60000
     }
   }
